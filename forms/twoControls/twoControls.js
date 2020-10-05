7d40bb6bd98a450b1432542f3f08d157f72d8e42
benmnielsen <BenNielsen@creighton.edu>
2020-10-04 22:14:14 -0500

    Initial commit

---
+++ forms/twoControls/twoControls.js
@@ -0,0 +1,14 @@
+/* 
+Change Header1 color. 
+*/ 
+btntwoControls.onclick=function(){
+  hdr1.style.backgroundColor = "#228B22"
+}
+
+/* 
+Add a message when the user clicks the switch. 
+*/ 
+
+swtch1.onchange=function(){
+  lbl1.textContent = "You have switched!"
+}
